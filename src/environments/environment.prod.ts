5003518581caac528841e0154160c37b6669d93f
Nirmalya <im.nirmalyadhar@gmail.com>
2022-04-29 13:32:39 +0530

    build files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+export const serviceBaseUrl =  'https://api.oak-alerts.com/alert/api/v1/';
+export const serviceLoginUrl = 'https://api.oak-insights.com/';
